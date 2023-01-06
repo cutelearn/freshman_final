0c54d6e76c156330bed080aacc64aed75e683fa3
yang-lin94 <casongg@gmail.com>
2023-01-06 13:28:30 +0800

    unit2

--- unit2/main.py
+++ unit2/main.py
@@ -1,17 +1,27 @@
 import csv
 import re
+import matplotlib.pyplot as plt
 
 def read_csv(filename):
-    data = []
     with open(filename, "r", encoding= "utf-8") as f:
         reader = csv.reader(f)
-        for row in reader:
-            data.append(row)
+        data = [row for row in reader]
     return data
 
-def regular_mouth(data):
-    for day in data:
-        mouth = re.search(r"/(\d+)/", day[0]).group(1)
+def regular_year_and_mouth(data, row, year, mouth):
+    num = []
+    date_pattern = rf"{year}/{mouth}/\d+"
+    for pattern in data:
+        date = re.search(date_pattern, pattern[0])
+        if date:
+            for i, sublist in enumerate(data):
+                if date.group(0) in sublist:
+                    # print(f'[{i}][{j}]')
+                    num.append(float(data[i][row]))
+                    # print(data[i][1])
+
+    print(sum(num))
+
 
 def calculation_date(data):
     date = []
@@ -19,9 +29,18 @@ def calculation_date(data):
         date.append(data[i][0])
     return date
 
-def main():
+def chart_generation(**kwargs):
     pass
 
-if __name__ == '__main__':
+def main():
     data = read_csv('covid19_tw_specimen.csv')
-    print(data)
+    regular_year_and_mouth(data,1,2020,1)
+    # regular_year_and_mouth(data)
+    # sublist = next(sublist for sublist in data if "2020/12/2" in sublist)
+    # print(sublist.index("2020/12/2"))
+    # for i, sublist in enumerate(data):
+    #     if "2020/12/5" in sublist:
+    #         j = sublist.index("2020/12/5")
+    #         print(f'[{i}][{j}]')
+if __name__ == '__main__':
+    main()
